c72ea9ff69d8a21a7351f7640e296815b0e8336d
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-09-22 13:32:43 +0530

    Internal links works with multiple language

--- src/utils/langs.js
+++ src/utils/langs.js
@@ -1,3 +1,6 @@
+//You can have the languages in an utils file so it can be reused.
 let languages = ['en', 'es']
-
-export { languages }
+function getTransLink(language, slug) {
+  return language === 'en' ? slug : `/${language}${slug}`
+}
+export { getTransLink, languages }
