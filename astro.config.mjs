6e1da77185bd8817fa07f6328f2b047970275f99
Matt Dunbar <me@mattdunbar.io>
2024-09-23 07:37:18 -0400

    Update template to match YouTube tutorial and add example env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,37 @@
 import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import tailwind from '@astrojs/tailwind'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+import storyblok from '@storyblok/astro'
+
+const { STORYBLOK_LOCAL, STORYBLOK_ACCESS_TOKEN, STORYBLOK_REGION } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
+const isLocal = STORYBLOK_LOCAL === 'yes'
+const localConfig = {
+	server: {
+		https: true,
+	},
+	vite: {
+		plugins: [mkcert()],
+	},
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        teaser: 'storyblok/Teaser',
-      },
-    }),
-    tailwind(),
-  ],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
+	integrations: [
+		storyblok({
+			accessToken: STORYBLOK_ACCESS_TOKEN,
+			apiOptions: {
+				region: STORYBLOK_REGION,
+			},
+			components: {
+				page: 'storyblok/Page',
+				feature: 'storyblok/Feature',
+				grid: 'storyblok/Grid',
+				teaser: 'storyblok/Teaser',
+			},
+		}),
+		tailwind(),
+	],
+	...(isLocal && localConfig),
 })
